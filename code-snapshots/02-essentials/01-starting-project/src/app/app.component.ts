5b46fbdbc6216b4fe9ec27251881b868346b4f73
Doa Bonifacio <50482180+doa-b@users.noreply.github.com>
2026-09-11 12:03:11 +0200

    2.31 defububg component inputs

--- code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
+import {DUMMY_USERS} from "./dummy-users";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    HeaderComponent, // auto-registers this component and uses it
+    HeaderComponent,
     UserComponent,
   ],
   templateUrl: './app.component.html', // markup
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  // 1. expose the imported data into its template
+  users = DUMMY_USERS;
+}
--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import {Component, computed, signal} from '@angular/core';
-
-import {DUMMY_USERS} from "../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,19 +7,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // create a signal value and store it in a property
-  // Initial value can be passed to the signal function
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  // we retrieve the computed value. Angular sets up a subscribtion to changes for you
-  imagePath = computed (() => 'assets/users/' + this.selectedUser().avatar)
+  // 3. mark it as settable from the outside
+  // 3b. tell typescript with ! that we know it will be initialized
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  // get imagePath(): string {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  // 4. re-instate your getter
+  get imagePath() {
+    return 'assets/users/' + this.avatar
+  }
 
   onSelectUser() {
-    const randomIndex2 = Math.floor(Math.random() * DUMMY_USERS.length);
-    // update the signal
-    this.selectedUser.set(DUMMY_USERS[randomIndex2]);
+    // do something
   }
 }
