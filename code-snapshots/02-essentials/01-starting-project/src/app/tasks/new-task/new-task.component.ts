b43d12b420cec518d7a336f7d6c12da974b42c07
Doa Bonifacio <50482180+doa-b@users.noreply.github.com>
2026-09-22 12:16:06 +0200

    59: Getting started with Services and dependancy injection

--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Output, inject, Input} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {NewTask} from "../task/task.model";
+import {TasksService} from "../tasks.service";
 
 @Component({
   selector: 'app-new-task',
@@ -12,25 +13,28 @@ import {NewTask} from "../task/task.model";
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  // 2. Add a new output
-  @Output() add = new EventEmitter<NewTask>()
+  @Input({required: true}) userId!: string
+  @Output() close = new EventEmitter<void>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
+  // 6 Alternative way of injection without constructor
+  private tasksService = inject(TasksService)
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
- // 3 Add a handler that creates and emits a new task
   onSubmit() {
     const newTask: NewTask = {
       title: this.enteredTitle,
       summary: this.enteredSummary,
       dueDate: this.enteredDate,
     }
-
-    this.add.emit(newTask);
+    // 7. Now we can use the service instead of emitting an event to add a task
+    // this.add.emit(newTask);
+    this.tasksService.addTask(newTask, this.userId);
+    // We DO need to send a close event though
+    this.close.emit();
   }
 }
--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
 import {Component, Input} from '@angular/core';
 import {TaskComponent} from "./task/task.component";
 import {User} from "../user/user.model";
-import {NewTask, Task} from "./task/task.model";
 import {NewTaskComponent} from "./new-task/new-task.component";
+import {TasksService} from "./tasks.service";
 
 @Component({
   selector: 'app-tasks',
@@ -17,57 +17,31 @@ import {NewTaskComponent} from "./new-task/new-task.component";
 export class TasksComponent {
   @Input({required: true}) name!: User['name'];
   @Input({required: true}) userId!: User['id'];
-  // 1. Add a flag to conditionally "open" the new task component
+
   isAddingTask = false;
-  tasks: Array<Task> = [{
-    id: 't1',
-    userId: 'u1',
-    title: 'Master Angular',
-    summary:
-      'Learn all the basic and advanced features of Angular & how to apply them.',
-    dueDate: '2025-12-31',
-  },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ]
+
+  // 4. Let Angular take care of dependancy injection of services
+  // You tell Angular which type of value you need and Angular creates it and provides it as an argument
+  // by adding the private (or public) keyword here, it will auto create a property of the same name
+  constructor(private taskService: TasksService) {}
+
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId)
+    // 5. Use your service
+    return this.taskService.getUserTasks(this.userId);
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id)
+    // 5. use your service
+    this.taskService.removeTask(id);
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  // 1. Add a way to cancel the dialog
-  onCancelAddTask() {
-    this.isAddingTask = false;
-  }
 
-  // 6. The handler adds userId and id and pushes it into the tasks array to store it
-  onAddTask(newTask: NewTask) {
-    this.tasks.push({
-      userId: this.userId,
-      id: new Date().toString(),
-      ...newTask
-    });
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }
